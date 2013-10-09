052c3118f852b3dcec87734ed440a3c219c4906e
Wes Morgan <cap10morgan@gmail.com>
2013-10-08 18:09:02 -0600

    convert to MELPA deps and update to Emacs Live 1.0-BETA-23 versions

---
+++ foundation-pack-pkg.el
@@ -0,0 +1,17 @@
+(define-package "foundation-pack" "1.0-BETA-23"
+  "Emacs Live pack providing the foundation stone on top of which all packs are built. Sets the visual theme."
+  '((auto-compile "1.0.10")
+    (buffer-move "0.4")
+    (dash "2.2.0")
+    (elisp-slime-nav "0")
+    (exec-path-from-shell "0")
+    (find-file-in-project "3.2")
+    (flx "0.1")
+    (idomenu "0")
+    (key-chord "0.5")
+    (pkg-info "0.2-cvs")
+    (popwin "0.7.0alpha")
+    (smex "2.1")
+    (smooth-scrolling "1.10")
+    (win-switch "1.0.6")
+    (window-number "0")))
